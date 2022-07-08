5fcb04c5820d35cc48698ce2890c2bdab008f0c7
Charanvir <charanvir123@gmail.com>
2022-07-07 23:11:47 -0400

    implemented routing and React Router and integrated more full stack functionality:

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+// BrowserRouter, Routes and Route are components that the React Router library provides
+// BrowserRouter is being renamed as Router in this case
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Singup from './pages/Signup';
 
 const httpLink = createHttpLink({
   // uniform resource identifier
@@ -19,13 +27,31 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      {/* makes all of the child components aware of the client-side routing that can take place now */}
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header></Header>
+          <div className='container'>
+            {/* Routes can hold several Route components */}
+            <Routes>
+              <Route path='/' element={<Home></Home>}></Route>
+              <Route path='/login' element={<Login></Login>}></Route>
+              <Route path='/signup' element={<Singup></Singup>}></Route>
+
+              <Route path="/profile">
+                {/* when profile is routed to, it will check for a username */}
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+
+              <Route path='/thought/:id' element={<SingleThought></SingleThought>}></Route>
+
+              <Route path='*' element={<NoMatch></NoMatch>}></Route>
+            </Routes>
+          </div>
+          <Footer></Footer>
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
